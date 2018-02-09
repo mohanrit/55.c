2fd5026b6f4e86dc1c31e62ae55a194705cd0a5f
mohanrit <35220535+mohanrit@users.noreply.github.com>
2018-02-09 14:11:43 +0530

    Create new file

---
+++ 37.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void swap(int,int);
+void main()
+{
+    int a=5,b=9;
+    
+    swap(a,b);
+    printf("%d%d\n",a,b);
+}
+void swap(int x,int y)
+{
+    int temp;
+    temp=x;x=y;temp=y;
+    printf("%d %d\n",x,y);
+}
+    
+        
+    
